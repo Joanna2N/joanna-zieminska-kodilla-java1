a5c3b7f0ce58dbd426ef709de15e33f1aea83a9b
Joanna Zieminska <joanna.zieminska2@gmail.com>
2022-02-20 22:55:39 +0200

    First commit

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -2,19 +2,18 @@ package com.kodilla.kodillacourse;
 
 public class Calculator {
 
-    int a = 5;
-    int b = 17;
-
-    // pierwsza metoda, dodawanie
     public int addAToB(int a, int b) {
         return a + b;
     }
 
-    // druga metoda - odejmowanie
     public int subtractAFromB(int a, int b) {
         return b - a;
+    }
 
-        // (bez tworzenia obiektu i deklarowania argumentów)
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        int sumResult = calculator.addAToB(5, 17);
 
+        System.out.println(sumResult);
     }
     }
