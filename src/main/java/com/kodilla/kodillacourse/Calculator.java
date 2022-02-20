3fdabea6384fa82b3921a7bbdc4f649aa867d055
Joanna Zieminska <joanna.zieminska2@gmail.com>
2022-02-20 21:49:40 +0200

    First commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+
+    int a = 5;
+    int b = 17;
+
+    // pierwsza metoda, dodawanie
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    // druga metoda - odejmowanie
+    public int subtractAFromB(int a, int b) {
+        return b - a;
+    }
+
+    }
